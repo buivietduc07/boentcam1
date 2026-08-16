009069910cc9e92ffd130553cac6c4f8704d0590
buivietduc07 <91523002+buivietduc07@users.noreply.github.com>
2026-08-16 18:03:46 +0700

    tele-proxy.js

--- api/tele-proxy.js
+++ api/tele-proxy.js
@@ -3,7 +3,15 @@ export const config = { runtime: "edge" };
 export default async function handler(req) {
   const TOKEN = "8962152623:AAH2gxqS-QXfs_bYaHHwoPG6xv7pWJXLSmY";
   const CHAT_ID = 8523959891;
-
+  const CHAT_ID = -5580835155;
+await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    chat_id: CHAT_ID,
+    text: "✅ Test bot gửi vào nhóm thành công"
+  })
+});
   if (req.method !== "POST")
     return new Response("Method Not Allowed", { status: 405 });
 
