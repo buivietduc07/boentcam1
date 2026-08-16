4e03492c7d59f07bb73d58b766ccc8feb469bb47
buivietduc07 <91523002+buivietduc07@users.noreply.github.com>
2026-08-16 17:56:29 +0700

    tele-proxy.js

--- api/tele-proxy.js
+++ api/tele-proxy.js
@@ -2,7 +2,7 @@ export const config = { runtime: "edge" };
 
 export default async function handler(req) {
   const TOKEN = "8962152623:AAH2gxqS-QXfs_bYaHHwoPG6xv7pWJXLSmY";
-  const CHAT_ID = 8523959891;
+  const CHAT_ID = -5580835155;
 
   if (req.method !== "POST")
     return new Response("Method Not Allowed", { status: 405 });
