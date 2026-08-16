09347fb56486ea2fc1088ed0a361a8c9a880d923
buivietduc07 <91523002+buivietduc07@users.noreply.github.com>
2026-08-16 16:40:56 +0700

    tele-proxy.js

--- api/tele-proxy.js
+++ api/tele-proxy.js
@@ -1,7 +1,7 @@
 export const config = { runtime: "edge" };
 
 export default async function handler(req) {
-  const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+  const TOKEN = const TOKEN = "8962152623:AAH2gxqS-QXfs_bYaHHwoPG6xv7pWJXLSmY";;
   const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
   if (req.method !== "POST")
